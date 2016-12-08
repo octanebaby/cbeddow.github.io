9314b982a031c41d4875d558036a2579d4b95e51
Christopher Beddow <christopher@mapillary.com>
2016-12-08 16:27:25 +0100

    Add files via upload

--- bsas/toggle.js
+++ bsas/toggle.js
@@ -1,23 +1,25 @@
-var toggleableLayerIds = [ 'temp-layer', 'trees-layer', 'ndvi-layer', 'mapillary' ];
+var toggleableLayerIds = [ 'Temperature', 'Tree Density', 'Vegetation', 'Mapillary', 'Tree Images' ];
+var colors = ['#221fb7', '#117302', '#ffff80', '#35af6d', 'magenta'];
 
 for (var i = 0; i < toggleableLayerIds.length; i++) {
     var id = toggleableLayerIds[i];
+    var col = colors[i];
 
     var link = document.createElement('a');
     link.href = '#';
-    link.className = 'active';
+    link.className = '';
     link.textContent = id;
 
     link.onclick = function (e) {
         var clickedLayer = this.textContent;
         e.preventDefault();
         e.stopPropagation();
-
         var visibility = map.getLayoutProperty(clickedLayer, 'visibility');
 
         if (visibility === 'visible') {
             map.setLayoutProperty(clickedLayer, 'visibility', 'none');
             this.className = '';
+
         } else {
             this.className = 'active';
             map.setLayoutProperty(clickedLayer, 'visibility', 'visible');
