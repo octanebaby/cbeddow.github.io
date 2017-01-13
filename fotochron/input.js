a7affe6c7efe3a26367f8806c2f6a6fcdf76f025
Christopher Beddow <christopher@mapillary.com>
2017-01-12 17:34:33 -0700

    Add files via upload

---
+++ fotochron/input.js
@@ -0,0 +1,9 @@
+var images = ["Ul-D12gQYR4RKKtcqmqsDQ","uAhuDQSHVSXRCmP5eqHmGg","_vXXqJhvPKbhyMCDyFsA3Q", "A-3lBUJhPkAwqlFnIMW1hg"];
+var currentImage = images[0];
+var project_title = "Beautiful Places"
+var project_subtitle = "A selection of beautiful Mapillary sequences from across North America"
+var titles = ["Salt Lake City", "Vancouver", "California", "South Dakota"]
+var content = ["The capital city of Utah, Salt Lake City is situated at the foot of the Wasatch Mountains and the Great Salt Lake. The city is close to a variety of environments, ranging from alpine peaks to pine forest to red desert. It was also the host of the 2002 Winter Olympics, and is the center of the Church of Jesus Christ of Latter Day Saints.",
+"Known for its mountains and rain, Vancouver is a port city on the Pacific coast of Canada. It was named or British Admiral George Vancouver, who sailed along the coast in the late 1700s. Vancouver hosted the 2010 Winter Olympics, and is close by the world famous Whistler Mountain Resort.",
+"Rolling green hills and satellite dishs on the horizon epitomize the clash of natural beauty and cutting edge technology in the Bay Area. Located east of Silicon Valley, this scene from the San Jose hills shows rare open space in a rapidly growing population center.",
+"One of the last elevated areas before the foothills of the Rocky Mountains give way to the great plains, the Black Hills area of South Dakota is pristine and isolated. However, nearby Mount Rushmore is a major tourist destination, while the spirit of the West lives on just down the highway in the town of Deadwood."]
